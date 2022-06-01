9374d93c9b43312d4e3ae44d3ef46ba5d5acbe37
AlexPap <arpappagallo@gmail.com>
2022-06-01 14:17:49 -0400

    added put/post/delete to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { reset } = require('nodemon');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -30,6 +31,7 @@ router.get('/:id', (req, res) => {
       id: req.params.id
     },
     attributs: ['id', 'tag_name'],
+    // be sure to include its associated Product data
     include: [
       {
         model: Product,
@@ -48,19 +50,59 @@ router.get('/:id', (req, res) => {
     console.log(err);
     res.status(500).jason(err);
   });
-  // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    id: req.body.id,
+    tag_name: req.body.tag_name
+  })
+  .then(dbTagData => reset.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData[0]) {
+      res.status(404).json({message: 'No Tag Found Under That Id'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destory({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({message: 'No Tag Found Under This Id'});
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
